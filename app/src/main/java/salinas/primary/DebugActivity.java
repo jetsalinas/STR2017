79328aa4f83fb4982a0f7332bbec5fe286e44e05
Jose Enrico Salinas <joseenrico.salinas@gmail.com>
2017-04-13 19:06:55 +0800

    Linked XML components to DebugActivity

--- app/src/main/java/salinas/primary/DebugActivity.java
+++ app/src/main/java/salinas/primary/DebugActivity.java
@@ -1,10 +1,13 @@
 package salinas.primary;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import android.app.Activity;
+import android.widget.TextView;
 
 import salinas.primary.data.UserParameterString;
 import salinas.primary.randomdata.RandomDataService;
@@ -15,8 +18,17 @@ import salinas.primary.randomdata.RandomDataService;
 
 public class DebugActivity extends Activity {
 
+    TextView valuesLocation = (TextView)findViewById(R.id.randomlocationvalue);
+    TextView valuesHumidity = (TextView)findViewById(R.id.randomhumidityvalue;
+    TextView valuesLight = (TextView)findViewById(R.id.randomlightvalue);
+    TextView valuesPressure = (TextView)findViewById(R.id.randompressurevalue);
+    TextView valuesTemperature = (TextView)findViewById(R.id.randomtemperaturevalue);
+    TextView valuesUserID = (TextView)findViewById(R.id.randomuserid);
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
 
