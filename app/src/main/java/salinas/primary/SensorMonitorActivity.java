7cdd9a696c715da2c807e19a413147ef5ff75d80
Jose Enrico Salinas <joseenrico.salinas@gmail.com>
2017-04-14 18:24:15 +0800

    Created SensorMonitorActivity and Layout

---
+++ app/src/main/java/salinas/primary/SensorMonitorActivity.java
@@ -0,0 +1,12 @@
+package salinas.primary;
+
+import android.app.Activity;
+
+/**
+ * Created by Jose Salinas on 4/14/2017.
+ */
+
+public class SensorMonitorActivity extends Activity {
+
+
+}
---
+++ app/src/main/java/salinas/primary/sensors/SensorService.java
@@ -0,0 +1,16 @@
+package salinas.primary.sensors;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Jose Salinas on 4/14/2017.
+ */
+
+public class SensorService extends IntentService {
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
