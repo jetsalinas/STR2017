08d3fa35dab418175f4fc42df2ee5454c4008869
Jose Enrico Salinas <joseenrico.salinas@gmail.com>
2017-04-15 16:11:21 +0800

    Pre-refactor commit

---
+++ app/src/main/java/salinas/primary/location/Keys.java
@@ -0,0 +1,11 @@
+package salinas.primary.location;
+
+/**
+ * Created by Jose Salinas on 4/15/2017.
+ */
+
+public class Keys {
+
+    public static final String GOOGLE_API_KEY = "AIzaSyCRJop6IO5l303SP-85gAUpqlP0l1MZP18";
+
+}
