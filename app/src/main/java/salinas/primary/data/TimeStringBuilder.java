5844761ef0be77a3c3acaa94172e8e524439f2f3
Jose Enrico Salinas <joseenrico.salinas@gmail.com>
2017-04-20 11:49:06 +0800

    Created intentservice to handle time broadcasting

---
+++ app/src/main/java/salinas/primary/data/TimeStringBuilder.java
@@ -0,0 +1,8 @@
+package salinas.primary.data;
+
+/**
+ * Created by Jose Salinas on 4/20/2017.
+ */
+
+public class TimeStringBuilder {
+}
---
+++ app/src/main/java/salinas/primary/time/Constants.java
@@ -0,0 +1,11 @@
+package salinas.primary.time;
+
+/**
+ * Created by Jose Salinas on 4/20/2017.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_TIME_DATA = "BROADCAST_TIME_DATA";
+    public static final String TIME_DATA_STRING = "TIME_DATA_STRING";
+}
